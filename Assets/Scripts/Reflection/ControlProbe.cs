742cc55ecd118f57fe87395e55d89ebd0d469c49
Trevor Westphal <trevorawestphal@gmail.com>
2015-11-23 09:06:26 -0700

    REverted mirror code - new code was more performance intensive

--- Assets/Scripts/Reflection/ControlProbe.cs
+++ Assets/Scripts/Reflection/ControlProbe.cs
@@ -3,23 +3,18 @@ using System.Collections;
 
 public class ControlProbe : MonoBehaviour {
 
-	public GameObject characterBody;
-	public GameObject characterCamera;
+	public GameObject plane;
+	public GameObject character;
 
 	void FixedUpdate () {
-		// Get distance to the floor
-		RaycastHit hit;
-		if(Physics.Raycast(characterBody.transform.position, Vector3.down, out hit)){
-			float ground_height = hit.point.y;
-			float distance_to_ground = characterCamera.transform.position.y - ground_height;
+		float offset = plane.transform.position.y - character.transform.position.y;
 
-			Vector3 new_position = new Vector3(
-				characterCamera.transform.position.x,
-				hit.point.y - distance_to_ground,
-				characterCamera.transform.position.z
-			);
+		Vector3 new_position = new Vector3(
+			character.transform.position.x,
+			plane.transform.position.y + offset,
+			character.transform.position.z
+		);
 
-			transform.position = new_position;
-		}
+		transform.position = new_position;
 	}
 }
