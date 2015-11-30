6d7079b8633545d3db9509de65048f012fe18b8f
Trevor Westphal <trevorawestphal@gmail.com>
2015-11-30 09:00:51 -0700

    Added mouse cursor hiding

---
+++ Assets/Scripts/Player Interaction/FirstPersonMouseFixes.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonMouseFixes : MonoBehaviour {
+
+	private bool lockedAndHidden;
+
+	void Start () {
+		lockedAndHidden = true;
+		setCursorState();
+	}
+
+	void FixedUpdate(){
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			lockedAndHidden = !lockedAndHidden;
+			setCursorState();
+		}
+	}
+
+	void setCursorState(){
+		Cursor.visible = !lockedAndHidden;
+		Cursor.lockState = lockedAndHidden ? CursorLockMode.Locked : CursorLockMode.Confined;
+	}
+}
