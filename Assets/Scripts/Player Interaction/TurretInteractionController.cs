236c03297a6064bdc37f5eecff178e4dc3be4d4c
Trevor Westphal <trevorawestphal@gmail.com>
2015-12-03 17:18:09 -0700

    Cleaned up turret controller code

--- Assets/Scripts/Player Interaction/TurretInteractionController.cs
+++ Assets/Scripts/Player Interaction/TurretInteractionController.cs
@@ -24,6 +24,7 @@ public class TurretInteractionController : MonoBehaviour {
 	public float projectileSpeed;
 	public float weaponCooldown;
 
+	// Private Variables
 	private float RAYCAST_LENGTH = 10.0f;
 	private bool playerInTurret;
 	private float rotationY = 0.0f;
@@ -34,48 +35,87 @@ public class TurretInteractionController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		// TODO clean up this shit
-		if(Input.GetMouseButton(0) && playerInTurret == true && Time.time - lastShotTime > weaponCooldown){
-			GameObject bolt = Instantiate(laserBolt);
-			bolt.transform.rotation = transform.rotation;
-			bolt.transform.position = projectileEmissionPoint.transform.position;
+		if(playerInTurret){
+			handleRotation();
 
-			lastShotTime = Time.time;
-
-			bolt.GetComponent<Rigidbody>().velocity = bolt.transform.right * projectileSpeed;
-			Destroy(bolt, 2.0f);
-		}
-
-		if(Input.GetMouseButtonDown(0) && playerInTurret == false){
-			RaycastHit hit;
-			Ray click_ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
-
-			if(Physics.Raycast(click_ray, out hit, RAYCAST_LENGTH) ){
-				if(hit.collider.gameObject == turretObject){
-					print("Clicked turret");
-					turretCamera.GetComponent<Camera>().enabled = true;
-					playerCamera.GetComponent<Camera>().enabled = false;
-					playerInTurret = true;
-				}
+			if(Input.GetMouseButton(0) && canFireWeapon()){
+				fireWeapon();
 			}
 		}
 
-
+		if(Input.GetMouseButtonDown(0) && playerInTurret == false && playerClickedOnTurret()){
+			enterTurret();
+		}
 
 		if(Input.GetKey("q") && playerInTurret == true){
-			turretCamera.GetComponent<Camera>().enabled = false;
-			playerCamera.GetComponent<Camera>().enabled = true;
-			playerInTurret = false;
+			exitTurret();
 		}
+	}
 
-		if(playerInTurret){
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+	//####################################################################
+	// Actions
+	//####################################################################
 
-            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-            rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+	void fireWeapon(){
+		GameObject bolt = Instantiate(laserBolt);
+		bolt.transform.rotation = transform.rotation;
+		bolt.transform.position = projectileEmissionPoint.transform.position;
 
-            transform.localEulerAngles = new Vector3(0, rotationX, rotationY);
+		lastShotTime = Time.time;
+
+		bolt.GetComponent<Rigidbody>().velocity = bolt.transform.right * projectileSpeed;
+		Destroy(bolt, 2.0f);
+	}
+
+	void enterTurret(){
+		lastShotTime = Time.time;
+		playerInTurret = true;
+		setPlayerLocation();
+	}
+
+	void exitTurret(){
+		playerInTurret = false;
+		setPlayerLocation();
+	}
+
+	//####################################################################
+	// Handlers
+	//####################################################################
+
+	void handleRotation(){
+		float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+		transform.localEulerAngles = new Vector3(0, rotationX, rotationY);
+	}
+
+	//####################################################################
+	// Queries
+	//####################################################################
+
+	bool playerClickedOnTurret(){
+		RaycastHit hit;
+		Ray click_ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
+
+		if(Physics.Raycast(click_ray, out hit, RAYCAST_LENGTH)){
+			return hit.collider.gameObject == turretObject;
 		}
+		return false;
+	}
+
+	bool canFireWeapon(){
+		return Time.time - lastShotTime > weaponCooldown;
+	}
+
+	//####################################################################
+	// Helper Functions
+	//####################################################################
+
+	void setPlayerLocation(){
+		turretCamera.GetComponent<Camera>().enabled = playerInTurret;
+		playerCamera.GetComponent<Camera>().enabled = !playerInTurret;
 	}
 
 
