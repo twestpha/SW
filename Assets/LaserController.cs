326924b0f4d643e55f6a670e5237c918aff3c1ae
Trevor Westphal <trevorawestphal@gmail.com>
2015-12-05 23:26:55 -0700

    Added laser collider mesh, remodeled bulkhead collider meshes, and made small tweaks to laser collision code, making it more responsive and realistic.

--- Assets/LaserController.cs
+++ Assets/LaserController.cs
@@ -5,19 +5,13 @@ public class LaserController : MonoBehaviour {
 
 	public GameObject particleSystemHit;
 
-	void Start(){
-	}
-
-	void FixedUpdate(){
-	}
-
-	void OnTriggerEnter(Collider collider){
-        if(collider.gameObject.tag == "Interior (Collider)"){
+	void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.tag == "Interior (Collider)"){
 			GameObject particles = Instantiate(particleSystemHit);
 			particles.transform.position = transform.position;
 
-            Destroy(gameObject);
 			Destroy(particles, 1.0f);
+			Destroy(this.gameObject);
         }
     }
 }
