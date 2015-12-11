f779ce2b4c6c8ef79dfd1a0362c29f65384efc5f
Trevor Westphal <trevorawestphal@gmail.com>
2015-12-10 23:47:55 -0700

    Added better scorch decal and removed decal system

--- Assets/Scripts/Effects/LaserController.cs
+++ Assets/Scripts/Effects/LaserController.cs
@@ -3,17 +3,54 @@ using System.Collections;
 
 public class LaserController : MonoBehaviour {
 
-	public GameObject[] particleSystemHits;
+	public GameObject hitExplosion;
+	public GameObject hitSparks;
+	public GameObject debris;
+	public GameObject hitDecal;
+
+	private Vector3 lastPosition;
+
+	void FixedUpdate(){
+		lastPosition = transform.position;
+	}
 
 	void OnCollisionEnter(Collision collision){
         if(collision.gameObject.tag == "Laser Collider"){
-			for(int i = 0; i < particleSystemHits.Length; ++i){
-				GameObject particles = Instantiate(particleSystemHits[i]);
-				particles.transform.position = transform.position;
 
-				Destroy(particles, 2.0f);
-				Destroy(this.gameObject);
+			// Explosion effect
+			GameObject explosion = Instantiate(hitExplosion);
+			explosion.transform.position = transform.position;
+			Destroy(explosion, 2.0f);
+
+			// Sparks effect
+			RaycastHit hit;
+			Vector3 direction = transform.position - lastPosition;
+			Vector3 reflection = direction;
+			Vector3 normal = Vector3.zero;
+			if(Physics.Raycast(lastPosition, direction, out hit)){
+				 reflection = Vector3.Reflect(direction, hit.normal);
+				 normal = hit.normal;
 			}
+
+			if(hitSparks){
+				GameObject sparks = Instantiate(hitSparks);
+				sparks.transform.position = transform.position;
+				sparks.transform.LookAt(sparks.transform.position + reflection);
+				Destroy(sparks, 2.0f);
+			}
+
+			if(hitDecal){
+				GameObject decal = Instantiate(hitDecal);
+				decal.transform.position = transform.position;
+				decal.transform.LookAt(direction);
+				decal.transform.Rotate(Vector3.right * 90.0f);
+
+				Debug.DrawRay(decal.transform.position, decal.transform.right * 10.0f, Color.red);
+
+				Destroy(decal, 10.0f);
+			}
+
+			Destroy(this.gameObject);
         }
     }
 }
